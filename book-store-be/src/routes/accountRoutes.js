63046882d1d4c30dd7e408283130218e13c25246
nvminh2003 <minhnv.works@gmail.com>
2025-06-05 00:59:46 +0700

    fix file

--- book-store-be/src/routes/accountRoutes.js
+++ book-store-be/src/routes/accountRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const accountController = require('../controllers/accountController');
+const accountController = require('../controllers/tem');
 const { checkAuthMiddleware } = require('../middleware/authMiddleware');
 
 
