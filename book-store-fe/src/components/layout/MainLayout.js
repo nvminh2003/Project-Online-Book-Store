bacfa4ac9b68ade73bf8097728e9455527994d42
nvminh2003 <minhnv.works@gmail.com>
2025-06-07 23:30:32 +0700

    Add notify component

--- book-store-fe/src/components/layout/MainLayout.js
+++ book-store-fe/src/components/layout/MainLayout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ToastManager } from "../common/ToastManager";
 
 const MainLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      <ToastManager />
       <Header />
       <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-6">
         {children}
